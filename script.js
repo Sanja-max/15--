63dffdabaf4d39558b52f9254a048c4a35dc8888
Sanja-max <sanjadj80@gmail.com>
2024-11-26 12:25:41 +0100

    ---

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Hello!');
+const submitButtun = document.querySelector("#submitButton");
+submitButtun.addEventListener("click", function (event){
+    event.preventDefault();
+    console.log("Button is clicked!");
+});
